7267eb44663365eeb3e3be2f8deac461b8ad2b16
Markitos95 <markitos@herrera.com>
2025-09-25 14:37:11 -0300

    modificacion inicio sesion

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { useLocation } from 'react-router-dom';
+
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
   return (
     <BrowserRouter>
       <Header />
+      {!isLoginPage && <Header />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
